8cd4b08c1b6d767fa20cd558fbe2d6e2afc2e670
alexcubor <sdiz@ya.ru>
2024-07-15 16:20:41 +0300

    init menu

--- plug-ins/USDzConvert/scripts/userSetup.py
+++ plug-ins/USDzConvert/scripts/userSetup.py
@@ -5,7 +5,7 @@ import maya.mel as mel
 def add_menu():
     # Create a new menu item
     gMainWindow = mel.eval('$temp1=$gMainWindow')
-    menu = cmds.menu('itsARMenu', label='ItsAR', tearOff=True, parent=gMainWindow)
+    menu = cmds.menu('usdzMenu', label='USDz', tearOff=True, parent=gMainWindow)
     # Add menu items to the new menu
     command = "import usdMayaExport; usdMayaExport.export_selection_to_usd()"
     cmds.menuItem(label="Export to USDZ", parent=menu, command=command)
