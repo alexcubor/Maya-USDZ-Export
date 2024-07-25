e400dc0290693c23210eff5fdb875ac6c3bc540d
alexcubor <sdiz@ya.ru>
2024-07-25 09:13:36 +0300

    Default exporting by animation timeline with flags skeleton on

--- plug-ins/USDzConvert/scripts/usdMayaExport.py
+++ plug-ins/USDzConvert/scripts/usdMayaExport.py
@@ -11,20 +11,26 @@ def export_selection_to_usdz():
     # Ensure the USD plugin is loaded
     if not cmds.pluginInfo('mayaUsdPlugin', query=True, loaded=True):
         cmds.loadPlugin('mayaUsdPlugin')
-    
+
     # Prompt the user for the export file path
     export_path = cmds.fileDialog2(fileMode=0, caption="Export USDZ File", fileFilter="USDZ Files (*.usdz)")
-    
+
     if not export_path:
         om.MGlobal.displayError("Export canceled.")
         return
 
     export_path = export_path[0]
     temp_path_usd = os.path.join(tempfile.gettempdir(), export_path.split('/')[-1].replace('usdz', 'usd'))
-    
+
     # Export the selection to USD
     try:
-        cmds.file(temp_path_usd, force=True, options=";", type="USD Export", exportSelected=True, preserveReferences=True)
+        start_frame = cmds.playbackOptions(q=True, min=True)
+        end_frame = cmds.playbackOptions(q=True, max=True)
+        cmds.optionVar(iv=('customFileDialogIncludeUVs', True))
+        options_str = ";exportUVs=1;exportSkels=auto;exportSkin=auto;exportBlendShapes=1;exportDisplayColor=0;filterTypes=nurbsCurve;exportColorSets=0;exportComponentTags=1;defaultMeshScheme=catmullClark;animation=1;eulerFilter=0;staticSingleSample=0;startTime={};endTime={};frameStride=1;frameSample=0.0;defaultUSDFormat=usdc;parentScope=;shadingMode=useRegistry;convertMaterialsTo=[UsdPreviewSurface];exportInstances=1;exportVisibility=1;mergeTransformAndShape=1;stripNamespaces=0;worldspace=0;materialsScopeName=mtl".format(
+            start_frame, end_frame)
+        cmds.file(temp_path_usd, force=True, options=options_str, type="USD Export", exportSelected=True, pr=True,
+                  es=True)
         try:
             tryProcess([temp_path_usd])
         except:
